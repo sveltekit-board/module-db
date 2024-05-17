02deb46e4ee31ef38a1ae94925e3bd4971221508
hotsixman <galactagun12@gmail.com>
2024-05-17 23:44:36 +0900

    add postgreSql

--- src/runQuery.ts
+++ src/runQuery.ts
@@ -1,67 +1,131 @@
 import mysql from 'mysql2';
+import { Client } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const database = process.env.DB_SERVICE ?? 'mysql';
 
 const option = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT !== undefined ? Number(process.env.DB_PORT) : database === "mysql" ? 3306 : 5432
 };
 
-export async function runQuery(callback:QueryCallback){
-    const db = mysql.createConnection(option);
-    db.connect();
+let runQuery: (callback: QueryCallback) => Promise<any>;
 
-    let queryFunction:QueryFunction = (query:string, values?:any[]) =>{
-        if(values){
-            return new Promise((res, rej) => {
-                db.query(query, values, (err, row) => {
-                    if(err){
-                        rej(err);
-                    }
-                    else{
-                        res(row);
-                    }
-                })
-            });
-        }
-        else{
-            return new Promise((res, rej) => {
-                db.query(query, (err, row) => {
-                    if(err){
-                        rej(err);
-                    }
-                    else{
-                        res(row);
-                    }
-                })
-            });
-        }
-    }
+if (database === "mysql") {
+    runQuery = async (callback: QueryCallback) => {
+        const db = mysql.createConnection(option);
+        db.connect();
 
-    let result;
-    let hasError:boolean = false;
-    try{
-        result = await callback(queryFunction);
-    }
-    catch(err){
-        result = err;
-        hasError = true;
-    }
-    finally{
-        db.destroy();
-    }
-    
-    if(hasError){
-        throw result;
-    }
-    else{
-        return result;
+        let queryFunction: QueryFunction = (query: string, values?: any[]) => {
+            if (values) {
+                return new Promise((res, rej) => {
+                    db.query(query, values, (err, row) => {
+                        if (err) {
+                            rej(err);
+                        }
+                        else {
+                            res(row);
+                        }
+                    })
+                });
+            }
+            else {
+                return new Promise((res, rej) => {
+                    db.query(query, (err, row) => {
+                        if (err) {
+                            rej(err);
+                        }
+                        else {
+                            res(row);
+                        }
+                    })
+                });
+            }
+        }
+
+        let result;
+        let hasError: boolean = false;
+        try {
+            result = await callback(queryFunction);
+        }
+        catch (err) {
+            result = err;
+            hasError = true;
+        }
+        finally {
+            db.destroy();
+        }
+
+        if (hasError) {
+            throw result;
+        }
+        else {
+            return result;
+        }
     }
 }
 
-type QueryCallback = (queryFunction:QueryFunction) => Promise<any>;
+if(database === "postgre"){
+    runQuery = async(callback: QueryCallback) => {
+        const client = new Client(option);
+        client.connect();
 
-type QueryFunction = ((query:string) => Promise<any>)|((query:string, values?:any[]) => Promise<any>);
+        let queryFunction: QueryFunction = (query: string, values?: any[]) => {
+            if (values) {
+                return new Promise((res, rej) => {
+                    client.query(query, values, (err, row) => {
+                        if (err) {
+                            rej(err);
+                        }
+                        else {
+                            res(row);
+                        }
+                    })
+                });
+            }
+            else {
+                return new Promise((res, rej) => {
+                    client.query(query, (err, row) => {
+                        if (err) {
+                            rej(err);
+                        }
+                        else {
+                            res(row);
+                        }
+                    })
+                });
+            }
+        }
+
+        let result;
+        let hasError: boolean = false;
+        try {
+            result = await callback(queryFunction);
+        }
+        catch (err) {
+            result = err;
+            hasError = true;
+        }
+        finally {
+            await client.end();
+        }
+
+        if (hasError) {
+            throw result;
+        }
+        else {
+            return result;
+        }
+    }
+}
+
+export {runQuery};
+
+type QueryCallback = (queryFunction: QueryFunction) => Promise<any>;
+
+type QueryFunction = ((query: string) => Promise<any>) | ((query: string, values?: any[]) => Promise<any>);
